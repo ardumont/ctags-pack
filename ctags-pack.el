fc85914fbb1705b439c6055ad884adffca5a8d55
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-06-17 15:27:44 +0200

    Refactor - Fix deleted code + Improve docstring

--- ctags-pack.el
+++ ctags-pack.el
@@ -5,12 +5,12 @@
 ;;; Code:
 
 (require 'install-packages-pack)
-(install-packs '(;; need org2blog as a glue between org-mode and wordpress
-                 ctags
+(install-packs '(magit
                  etags-select))
 
-(require 'ctags)
+(require 'etags)
 (require 'etags-select)
+(require 'magit)
 
 (defvar *CTAGS-BINARY* "ctags-exuberant"
   "Ctags binary to use for ctags generation.")
@@ -18,15 +18,62 @@
 (defvar *CTAGS-LOG-PREFIX* "ctags-pack - "
   "Prefix log message to explicit what triggers the TAGS.")
 
+(defun ctags-pack/set-project-tags-file-name ()
+  "If the current directory is a git project (and not the home directory).
+Make .tags the default tag file location.
+You probably want to add .tags to a .gitignore_global file."
+  (interactive)
+  (let* ((top-dir (magit-get-top-dir))
+         (home-dir (expand-file-name "~/"))
+         (tags-file (format "%sTAGS" top-dir)))
+    (when (and top-dir
+               (not (string-equal top-dir home-dir)))
+      (setq-local tags-table-list (list tags-file))
+      (message "Project tags file set to: %s" tags-file)
+      (generate-project-tags-if-missing))))
+
+(defun ctags-pack/generate-project-tags (tags-file)
+  "Generate tags for current project in `TAGS-FILE`."
+  (interactive)
+  (call-process *CTAGS-BINARY* nil nil nil
+                  "-Re"
+                  "--exclude=.git"
+                  "--exclude='.#*'"
+                  "--langmap=Lisp:+.clj.cljs"
+                  (format "-f %s" tags-file)
+                  (magit-get-top-dir)))
+
+(defun ctags-pack/get-project-tags-file-name ()
+  "Get file name to put project tags in."
+  (interactive)
+  (car tags-table-list))
+
+(defun ctags-pack/generate-project-tags-if-missing ()
+  "If `tags-table-list` is set and no project tags file exists, generate it."
+  (interactive)
+  (when tags-table-list
+    (let* ((tags-file (get-project-tags-file-name)))
+      (when (and tags-file (not (file-exists-p tags-file)))
+        (generate-project-tags tags-file)
+        (message "Generated new project tags in %s" tags-file)))))
+
+(defun ctags-pack/regenerate-project-tags ()
+  "If `tags-file-name` is set, regenerate the tags file."
+  (interactive)
+  (let* ((tags-file-name (get-project-tags-file-name)))
+    (when tags-file-name
+      (generate-project-tags tags-file-name)
+      (message "%sRegenerated project tags in %s" *CTAGS-LOG-PREFIX* tags-file-name))))
+
 ;; When a new file is found in a git repo, set the project tags filename.
-(add-hook 'find-file-hook 'set-project-tags-file-name)
+(remove-hook 'find-file-hook 'ctags-pack/set-project-tags-file-name)
 
 ;; When a new file is found in a git repo, generate the project tags file if it
 ;; doesn't exist.
-;;(add-hook 'find-file-hook 'generate-project-tags-if-missing)
+;;(add-hook 'find-file-hook 'ctags-pack/generate-project-tags-if-missing)
 
 ;; When a file is saved, (potentially) regenerate the tags file.
-(add-hook 'after-save-hook 'regenerate-project-tags)
+(remove-hook 'after-save-hook 'ctags-pack/regenerate-project-tags)
 
 ;; If the tags file is rewritten, pick up the changes without prompting.
 (setq tags-revert-without-query t)
